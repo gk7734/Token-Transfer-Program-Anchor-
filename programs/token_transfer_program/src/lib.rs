5b0ca1b01006b1935a476fa028536e44caf02e96
gk7734 <gamekiller7734@naver.com>
2024-10-31 23:36:52 +0900

    Add TransferAccounts Struct

--- programs/token_transfer_program/src/lib.rs
+++ programs/token_transfer_program/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Transfer, Token, TokenAccount};
 
 declare_id!("2JcXXExQa4jYjbfTbZH4LS3QRWvGXtnmsoNHV4hTdtov");
 
@@ -6,11 +7,22 @@ declare_id!("2JcXXExQa4jYjbfTbZH4LS3QRWvGXtnmsoNHV4hTdtov");
 pub mod token_transfer_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn transfer_tokens(ctx: Context<TransferAccounts>) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferAccounts<'info> {
+    #[account(mut)]
+    pub from: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub to: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>
+}
